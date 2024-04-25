a6762734cc9d63166887fd14d1b0c13a799cbdbd
Sebas <sebastian.gavvel@educa.jcyl.es>
2024-04-25 18:34:53 +0200

    Primera version de esta calculadora

---
+++ src/main/java/com/mycompany/tareacontrolversion/TareaControlVersion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareacontrolversion;
+
+/**
+ *
+ * @author admin
+ */
+public class TareaControlVersion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Este es el texto del resporitiorio local");
+        
+    }
+}
